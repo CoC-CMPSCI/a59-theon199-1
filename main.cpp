c35ea3cc16b9c1075be852c09e0dc9620191ffe1
Sricharan Vagvala <charansv19@gmail.com>
2026-04-07 14:26:47 -0700

    done.

--- main.cpp
+++ main.cpp
@@ -5,7 +5,20 @@ using namespace std;
 int main()
 {
 	int prev, current;
-	// TODO
+	bool first = true;
 
-	// TODO
+	cin >> current;
+	while (first || current <= prev)
+	{
+		first = false;
+		cout << current;
+		prev = current;
+		cin >> current;
+		if (current <= prev)
+		{
+			cout << '\t';
+		}
+	}
+
+	cout << endl;
 }
